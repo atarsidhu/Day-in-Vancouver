71b525e167811275440c0243872ba9565c80b8fb
Atar Sidhu <a.sidhu14@hotmail.com>
2021-01-20 22:22:56 -0800

    EventListeners created to modify the featured Attr/Rest among other minor changes

--- app.js
+++ app.js
@@ -65,42 +65,72 @@ function initMap() {
   }
 }
 
+// Define array where JSON data will be stored
+let attractionArray = [];
+let restaurantArray = [];
+
+let isAttraction = true;
+
 fetch("./web-scrapes/attractions.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data[0].name);
-    //document.querySelector(".attr-title").innerHTML = data[0].name;
+    // Retreive all the data and store it into an array
+    for (let i = 0; i < data.length; i++) {
+      attractionArray[i] = data[i];
+    }
 
     // Populate the attractions list
     let nameOfAttraction = "";
+
     data.forEach(function (attraction) {
       nameOfAttraction += `
-        <div class="card-other">
+        <div class="card-other-attr">
           ${attraction.name}
         </div>
       `;
     });
-    // document.getElementsByClassName("card-wrapper-other").innerHTML = nameOfAttraction;
     document.querySelector(".other-attr").innerHTML = nameOfAttraction;
 
     // Populate the featured attraction
     document.querySelector(".attr-title").innerHTML = data[0].name;
     document.querySelector(".attr-image").src = data[0]["image-src"];
     document.querySelector(".attr-desc").innerHTML = data[0]["desc-short"];
-    //document.querySelector(".attr-address").innerHTML = data[0].address;
+  })
+  .then(() => {
+    loadEventListeners(isAttraction);
   });
+
+let restImages = [];
 
 fetch("./web-scrapes/restaurants.json")
   .then((response) => response.json())
   .then((data) => {
+    // Retreive all the data and store it into an array
+    // restaurantArray[0] = data[0];
+    // let j = 0;
+    // for (let i = 1; i < data.length; i++) {
+    //   // WHY CANT WE DO FOREACH ARR WHERE NAME == NAME, ADD IMAGE OR SMTN
+    //   if (data[i].name === restaurantArray[j].name) {
+    //     restImages.push(data[i]["images-src"]);
+    //   } else {
+    //     j++;
+    //     restaurantArray[j] = data[i];
+    //   }
+    // }
+
+    for (let i = 0; i < data.length; i++) {
+      restaurantArray[i] = data[i];
+    }
+
+    console.log(restaurantArray);
+    // Get a list of restaurant names and remove duplicates
     const uniqueNames = [...new Set(data.map((item) => item.name))];
-    console.log(uniqueNames);
 
     // Populate the restaurants list
     let nameOfRestaurant = "";
     uniqueNames.forEach(function (restaurant) {
       nameOfRestaurant += `
-        <div class="card-other">
+        <div class="card-other-rest">
           ${restaurant}
         </div>
       `;
@@ -113,37 +143,61 @@ fetch("./web-scrapes/restaurants.json")
     document.querySelector(".rest-cuisine").innerHTML = data[0].cuisine;
     document.querySelector(".rest-menu").href = data[0]["menu-href"];
     document.querySelector(".rest-rating").innerHTML = data[0].rating;
+  })
+  .then(() => {
+    loadEventListenersRest();
   });
 
-function displayCard() {
-  // document.querySelectorAll(".acc__btn").forEach((button) => {
-  //   button.addEventListener("click", () => {
-  //     const accordianContent = button.nextElementSibling;
-  //     button.classList.toggle("acc__btn--active");
-  //     if (button.classList.contains("acc__btn--active")) {
-  //       accordianContent.style.maxHeight = accordianContent.scrollHeight + "px";
-  //     } else {
-  //       accordianContent.style.maxHeight = 0;
-  //     }
-  //   });
-  // });
+function loadEventListeners() {
+  let theDiv = document.getElementsByClassName("card-other-attr");
+  let name;
 
-  let acc = document.getElementsByClassName("acc__btn");
-  let i;
+  for (let i = 0; i < theDiv.length; i++) {
+    theDiv[i].addEventListener("click", (e) => {
+      name = e.target.innerText;
+      window.scrollTo(0, 0);
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-      const accordianContent = this.nextElementSibling;
-      this.classList.toggle("acc__btn--active");
-      if (this.classList.contains("acc__btn--active")) {
-        accordianContent.style.maxHeight = accordianContent.scrollHeight + "px";
-      } else {
-        accordianContent.style.maxHeight = 0;
+      for (let j = 0; j < attractionArray.length; j++) {
+        if (attractionArray[j].name === name) {
+          document.querySelector(".attr-title").innerHTML =
+            attractionArray[j].name;
+          document.querySelector(".attr-image").src =
+            attractionArray[j]["image-src"];
+
+          if (attractionArray[j]["desc-long"] == null) {
+            document.querySelector(".attr-desc").innerHTML =
+              attractionArray[j]["desc-short"];
+          } else {
+            document.querySelector(".attr-desc").innerHTML =
+              attractionArray[j]["desc-long"];
+          }
+        }
       }
     });
   }
-
-  console.log(acc.length);
 }
 
-displayCard();
+function loadEventListenersRest() {
+  let theDiv = document.getElementsByClassName("card-other-rest");
+  let name;
+
+  for (let i = 0; i < theDiv.length; i++) {
+    theDiv[i].addEventListener("click", (e) => {
+      name = e.target.innerText;
+      window.scrollTo(0, 0);
+
+      for (let j = 0; j < restaurantArray.length; j++) {
+        if (restaurantArray[j].name === name) {
+          document.querySelector(".rest-title").innerHTML =
+            restaurantArray[j].name;
+          document.querySelector(".rest-image").src =
+            restaurantArray[j]["images-src"];
+          document.querySelector(".rest-cuisine").innerHTML =
+            restaurantArray[j]["cuisine"];
+          document.querySelector(".rest-rating").innerHTML =
+            restaurantArray[j].rating;
+        }
+      }
+    });
+  }
+}
