56e9a24362244738d22be14595372305ea9308d2
Atar Sidhu <a.sidhu14@hotmail.com>
2021-01-29 16:36:09 -0800

    Search bar configured and working correctly

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 let map;
 let markerAttr;
 let markerRest;
-const markerIcon =
-  "https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red.png";
+//const markerIcon =
+//  "https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red.png";
+const markerIconA =
+  "data:image/svg+xml,%3Csvg%20version%3D%221.1%22%20width%3D%2227px%22%20height%3D%2243px%22%20viewBox%3D%220%200%2027%2043%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%3E%0A%3Cdefs%3E%0A%3Cpath%20id%3D%22a%22%20d%3D%22m12.5%200c-6.9039%200-12.5%205.5961-12.5%2012.5%200%201.8859%200.54297%203.7461%201.4414%205.4617%203.425%206.6156%2010.216%2013.566%2010.216%2022.195%200%200.46562%200.37734%200.84297%200.84297%200.84297s0.84297-0.37734%200.84297-0.84297c0-8.6289%206.7906-15.58%2010.216-22.195%200.89844-1.7156%201.4414-3.5758%201.4414-5.4617%200-6.9039-5.5961-12.5-12.5-12.5z%22%2F%3E%0A%3C%2Fdefs%3E%0A%3Cg%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%0A%3Cg%20transform%3D%22translate(1%201)%22%3E%0A%3Cuse%20fill%3D%22%23EA4335%22%20fill-rule%3D%22evenodd%22%20xlink%3Ahref%3D%22%23a%22%2F%3E%0A%3Cpath%20d%3D%22m12.5-0.5c7.18%200%2013%205.82%2013%2013%200%201.8995-0.52398%203.8328-1.4974%205.6916-0.91575%201.7688-1.0177%201.9307-4.169%206.7789-4.2579%206.5508-5.9907%2010.447-5.9907%2015.187%200%200.74177-0.6012%201.343-1.343%201.343s-1.343-0.6012-1.343-1.343c0-4.7396-1.7327-8.6358-5.9907-15.187-3.1512-4.8482-3.2532-5.01-4.1679-6.7768-0.97449-1.8608-1.4985-3.7942-1.4985-5.6937%200-7.18%205.82-13%2013-13z%22%20stroke%3D%22%23fff%22%2F%3E%0A%3C%2Fg%3E%0A%3Ctext%20text-anchor%3D%22middle%22%20dy%3D%220.3em%22%20x%3D%2214%22%20y%3D%2215%22%20font-family%3D%22Roboto%2C%20Arial%2C%20sans-serif%22%20font-size%3D%2216px%22%20fill%3D%22%23FFF%22%3EA%3C%2Ftext%3E%0A%3C%2Fg%3E%0A%3C%2Fsvg%3E%0A";
+const markerIconB =
+  "data:image/svg+xml,%3Csvg%20version%3D%221.1%22%20width%3D%2227px%22%20height%3D%2243px%22%20viewBox%3D%220%200%2027%2043%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%3E%0A%3Cdefs%3E%0A%3Cpath%20id%3D%22a%22%20d%3D%22m12.5%200c-6.9039%200-12.5%205.5961-12.5%2012.5%200%201.8859%200.54297%203.7461%201.4414%205.4617%203.425%206.6156%2010.216%2013.566%2010.216%2022.195%200%200.46562%200.37734%200.84297%200.84297%200.84297s0.84297-0.37734%200.84297-0.84297c0-8.6289%206.7906-15.58%2010.216-22.195%200.89844-1.7156%201.4414-3.5758%201.4414-5.4617%200-6.9039-5.5961-12.5-12.5-12.5z%22%2F%3E%0A%3C%2Fdefs%3E%0A%3Cg%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%0A%3Cg%20transform%3D%22translate(1%201)%22%3E%0A%3Cuse%20fill%3D%22%23EA4335%22%20fill-rule%3D%22evenodd%22%20xlink%3Ahref%3D%22%23a%22%2F%3E%0A%3Cpath%20d%3D%22m12.5-0.5c7.18%200%2013%205.82%2013%2013%200%201.8995-0.52398%203.8328-1.4974%205.6916-0.91575%201.7688-1.0177%201.9307-4.169%206.7789-4.2579%206.5508-5.9907%2010.447-5.9907%2015.187%200%200.74177-0.6012%201.343-1.343%201.343s-1.343-0.6012-1.343-1.343c0-4.7396-1.7327-8.6358-5.9907-15.187-3.1512-4.8482-3.2532-5.01-4.1679-6.7768-0.97449-1.8608-1.4985-3.7942-1.4985-5.6937%200-7.18%205.82-13%2013-13z%22%20stroke%3D%22%23fff%22%2F%3E%0A%3C%2Fg%3E%0A%3Ctext%20text-anchor%3D%22middle%22%20dy%3D%220.3em%22%20x%3D%2214%22%20y%3D%2215%22%20font-family%3D%22Roboto%2C%20Arial%2C%20sans-serif%22%20font-size%3D%2216px%22%20fill%3D%22%23FFF%22%3EB%3C%2Ftext%3E%0A%3C%2Fg%3E%0A%3C%2Fsvg%3E%0A";
 let streetAddressAttr = "";
 let streetAddressRest = "";
 let latLngBounds = [];
 let imageIdx = 0;
 let numberOfImages = 0;
 let directionsService;
-let directionsRenderer;
+let directionsDisplay;
+let infoWindow;
+let startingPointInput;
+let destinationPointInput;
+let marker;
 
 // Define array where JSON data will be stored
 let attractionArray = [];
@@ -24,9 +32,21 @@ function initMap() {
   // New Map
   map = new google.maps.Map(document.getElementById("map"), options);
 
-  let input = document.getElementById("search");
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  startingPointInput = document.getElementById("starting-search");
+  destinationPointInput = document.getElementById("destination-search");
+  configAutoComplete(startingPointInput);
+  configAutoComplete(destinationPointInput);
+  //map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  infoWindow = new google.maps.InfoWindow();
+  // directionsDisplay = new google.maps.DirectionsRenderer({
+  //   suppressMarkers: true,
+  // });
+}
+
+function configAutoComplete(input) {
   let autoComplete = new google.maps.places.Autocomplete(input);
   autoComplete.bindTo("bounds", map);
 
@@ -42,55 +62,28 @@ function initMap() {
       map.fitBounds(place.geometry.viewport);
     } else {
       map.setCenter(place.geometry.viewport);
-      //map.setZoom(17);
     }
 
-    markerAttr.setPosition(place.geometry.location);
-    markerAttr.setVisible(true);
-  });
+    let searchAddress = place.geometry.location;
 
-  //addMarker({ lat: 49.2844863, lng: -123.108996 });
-  //addMarker2({ lat: 49.2759736, lng: -123.0693516 });
-  latLngBounds = [
-    new google.maps.LatLng(49.2844863, -123.108996),
-    new google.maps.LatLng(49.2759736, -123.0693516),
-  ];
-
-  // Add marker function
-  function addMarker(coords) {
-    markerAttr = new google.maps.Marker({
-      position: coords,
-      map: map,
-    });
-
-    setMarkerIcon(markerAttr, "Steam Clock");
-  }
-
-  function addMarker2(coords) {
-    markerRest = new google.maps.Marker({
-      position: coords,
-      map: map,
-    });
-
-    setMarkerIcon(markerRest, "Absinthe Bistro");
-  }
-
-  directionsService = new google.maps.DirectionsService();
-  directionsRenderer = new google.maps.DirectionsRenderer();
-}
-
-function setMarkerIcon(markerType, name) {
-  markerType.setLabel({
-    color: "red",
-    fontWeight: "bold",
-    text: name,
-  });
-  markerType.setIcon({
-    labelOrigin: new google.maps.Point(11, 48),
-    url: markerIcon,
-    size: new google.maps.Size(30, 40),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(11, 40),
+    if (startingPointInput.value !== "" && destinationPointInput.value !== "") {
+      getDirections(searchAddress, input.id);
+    } else {
+      if (input.id === "starting-search") {
+        if (destinationPointInput.value === "") {
+          createMarker(searchAddress, markerIconA);
+        } else {
+          removeMarker();
+        }
+      } else {
+        if (startingPointInput.value === "") {
+          createMarker(searchAddress, markerIconB);
+        } else {
+          removeMarker();
+        }
+      }
+      getDirections(searchAddress, input.id);
+    }
   });
 }
 
@@ -181,6 +174,9 @@ function loadImages(num) {
   dots.firstChild.className = "dot active";
 }
 
+let selectedAttr = 0;
+let selectedRest = 0;
+
 function loadEventListeners() {
   const theDiv = document.getElementsByClassName("card-other-attr");
   let name;
@@ -189,6 +185,12 @@ function loadEventListeners() {
     theDiv[i].addEventListener("click", (e) => {
       name = e.target.innerText;
       window.scrollTo(0, 0);
+      startingPointInput.value = name;
+
+      // Change background of selected attr
+      theDiv[selectedAttr].className = "card-other-attr";
+      selectedAttr = i;
+      e.target.className = "card-other-attr selected";
 
       for (let j = 0; j < attractionArray.length; j++) {
         if (attractionArray[j].name === name) {
@@ -208,9 +210,8 @@ function loadEventListeners() {
           }
         }
       }
-      //geocode(streetAddressAttr, markerAttr, name);
-      //console.log(streetAddressAttr);
-      getDirections(streetAddressAttr);
+
+      getDirections(streetAddressAttr, name);
     });
   }
 }
@@ -226,6 +227,12 @@ function loadEventListenersRest() {
     theDiv[i].addEventListener("click", (e) => {
       name = e.target.innerText;
       window.scrollTo(0, 0);
+      destinationPointInput.value = name;
+
+      // Change background of selected rest
+      theDiv[selectedRest].className = "card-other-rest";
+      selectedRest = i;
+      e.target.className = "card-other-rest selected";
 
       for (let j = 0; j < restaurantArray.length; j++) {
         if (restaurantArray[j].name === name) {
@@ -247,8 +254,8 @@ function loadEventListenersRest() {
           loadImages(j);
         }
       }
-      //geocode(streetAddressRest, markerRest, name);
-      getDirections(streetAddressRest);
+
+      getDirections(streetAddressRest, name);
     });
   }
 
@@ -275,45 +282,12 @@ function loadEventListenersRest() {
   });
 }
 
-// function geocode(streetAddress, markerType, name) {
-//   axios
-//     .get("https://maps.googleapis.com/maps/api/geocode/json", {
-//       params: {
-//         address: streetAddress,
-//         key: "AIzaSyDoWBesVxVWPUv4CMbKqyMmNy5-YNZOlxs",
-//       },
-//     })
-//     .then(function (response) {
-//       //let latLng = response.data.results[0].geometry.location;
-//       //updateMarker(latLng, markerType, name);
-//       // let placeID = response.data.results[0].place_id;
-//       // distanceMatrix(placeID);
-//       //console.log(response.data.results[0].geometry.location);
-//       //console.log(streetAddress);
+let startingAddress = "";
+let destinationAddress = "";
+let startingName = "";
+let destinationName = "";
 
-//       //getDirections(streetAddress);
-//       // Check to see if name is an attraction name or restaurant name
-//       for (let i = 0; i < attractionArray.length; i++) {
-//         if (name === attractionArray[i].name) {
-//           let placeIdAttr = response.data.results[0].place_id;
-//           getDirections(placeIdAttr);
-//           break;
-//         }
-//         if (name === restaurantArray[i].name) {
-//           let placeIdRest = response.data.results[0].place_id;
-//           getDirections(placeIdRest);
-//           break;
-//         }
-//       }
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-// }
-let start = "305 Water St, Vancouver, British Columbia V6B 1B8 Canada";
-let end = "952 Commercial Dr, Vancouver, British Columbia V5L 3W7 Canada";
-
-function getDirections(streetAddress) {
+function getDirections(streetAddress, name) {
   // let proxyURL = "https://cors-anywhere.herokuapp.com/";
   // let url =
   //   "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:ChIJDWe3Lp1xhlQRFVVrQy7psuA&destination=place_id:ChIJOVXSPT5xhlQRZVWgqcWl4bY&key=AIzaSyDoWBesVxVWPUv4CMbKqyMmNy5-YNZOlxs";
@@ -325,59 +299,87 @@ function getDirections(streetAddress) {
   //   .catch(function (error) {
   //     console.log(error);
   //   });
-  let name = "";
 
   for (let i = 0; i < attractionArray.length; i++) {
     if (streetAddress === attractionArray[i].address) {
-      //let placeIdAttr = response.data.results[0].place_id;
-      name = attractionArray[i].name;
-      start = streetAddress;
+      startingName = name;
+      startingAddress = streetAddress;
       break;
     }
     if (streetAddress === restaurantArray[i].address) {
-      //let placeIdRest = response.data.results[0].place_id;
-      end = streetAddress;
+      destinationName = name;
+      destinationAddress = streetAddress;
       break;
+    }
+    if (i == 28) {
+      if (name === "starting-search") {
+        startingName = startingPointInput.value;
+        startingAddress = streetAddress;
+        console.log(startingName + " " + startingAddress);
+      } else {
+        destinationName = destinationPointInput.value;
+        destinationAddress = streetAddress;
+        console.log(destinationName + " " + destinationAddress);
+      }
     }
   }
 
-  directionsRenderer.setMap(map);
+  if (startingAddress !== "" && destinationAddress !== "") {
+    directionsDisplay.setMap(map);
 
-  let request = {
-    origin: start,
-    destination: end,
-    travelMode: google.maps.TravelMode.DRIVING,
-  };
+    let request = {
+      origin: startingAddress,
+      destination: destinationAddress,
+      travelMode: google.maps.TravelMode.DRIVING,
+    };
 
-  directionsService.route(request, function (response, status) {
-    if (status === "OK") {
-      directionsRenderer.setDirections(response);
-      console.log(response);
+    directionsService.route(request, function (response, status) {
+      if (status === "OK") {
+        directionsDisplay.setDirections(response);
+        let leg = response.routes[0].legs[0];
+        //infoWindow.close();
 
-      // markerType.setLabel({
-      //   color: "red",
-      //   fontWeight: "bold",
-      //   text: name,
-      // });
-      // markerType.setIcon({
-      //   labelOrigin: new google.maps.Point(11, 48),
-      //   url: markerIcon,
-      //   size: new google.maps.Size(30, 40),
-      //   origin: new google.maps.Point(0, 0),
-      //   anchor: new google.maps.Point(11, 40),
-      // });
+        let content = `<div class="info-window">
+        <h3 class="info-title">${startingName}</h3>
+        </div>`;
+        infoWindow.setContent(content);
+        infoWindow.setPosition(leg.start_location);
 
-      // let marker = new google.maps.Marker({
-      //   position: start,
-      //   map: map,
-      //   label: name
-      // });
-    }
+        leg.start_address = startingName;
+        leg.end_address = destinationName;
+      }
+    });
+
+    let panel = document.querySelector(".map-panel");
+    directionsDisplay.setPanel(panel);
+    //map.controls[google.maps.ControlPosition.LEFT_CENTER].push(panel);
+  }
+}
+
+function removeMarker() {
+  marker.setMap(null);
+}
+
+function createMarker(position, icon) {
+  marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    icon: icon,
   });
 
-  let panel = document.querySelector(".map-panel");
-  directionsRenderer.setPanel(panel);
-  //map.controls[google.maps.ControlPosition.LEFT_CENTER].push(panel);
+  // marker.setLabel({
+  //   color: "black",
+  //   fontWeight: "bold",
+  //   text: name,
+  // });
+
+  // marker.setIcon({
+  //   labelOrigin: new google.maps.Point(11, 48),
+  //   url: icon,
+  //   size: new google.maps.Size(30, 40),
+  //   origin: new google.maps.Point(0, 0),
+  //   anchor: new google.maps.Point(11, 40),
+  // });
 }
 
 // function updateMarker(latLng, markerType, name) {
@@ -396,57 +398,57 @@ function getDirections(streetAddress) {
 //   //   infoWindow.close();
 //   // });
 
-//   map.panTo(latLng);
+//   //map.panTo(latLng);
 //   //map.setZoom(14);
 
-//   let myStyle = [
-//     {
-//       featureType: "administrative",
-//       elementType: "labels",
-//       stylers: [{ visibility: "off" }],
-//     },
-//     {
-//       featureType: "poi",
-//       elementType: "labels",
-//       stylers: [{ visibility: "off" }],
-//     },
-//     {
-//       featureType: "road",
-//       elementType: "labels",
-//       stylers: [{ visibility: "on" }],
-//     },
-//     {
-//       featureType: "transit",
-//       elementType: "labels.icon",
-//       stylers: [{ visibility: "off" }],
-//     },
-//   ];
+//   // let myStyle = [
+//   //   {
+//   //     featureType: "administrative",
+//   //     elementType: "labels",
+//   //     stylers: [{ visibility: "off" }],
+//   //   },
+//   //   {
+//   //     featureType: "poi",
+//   //     elementType: "labels",
+//   //     stylers: [{ visibility: "off" }],
+//   //   },
+//   //   {
+//   //     featureType: "road",
+//   //     elementType: "labels",
+//   //     stylers: [{ visibility: "on" }],
+//   //   },
+//   //   {
+//   //     featureType: "transit",
+//   //     elementType: "labels.icon",
+//   //     stylers: [{ visibility: "off" }],
+//   //   },
+//   // ];
 
-//   map.setMapTypeId("mystyle");
-//   map.mapTypes.set(
-//     "mystyle",
-//     new google.maps.StyledMapType(myStyle, { name: "My Style" })
-//   );
+//   // map.setMapTypeId("mystyle");
+//   // map.mapTypes.set(
+//   //   "mystyle",
+//   //   new google.maps.StyledMapType(myStyle, { name: "My Style" })
+//   // );
 
-//   // Check to see if name is an attraction name or restaurant name
-//   for (let i = 0; i < attractionArray.length; i++) {
-//     if (name === attractionArray[i].name) {
-//       latLngBounds[0] = latLng;
-//       console.log("Attr: " + name);
-//       break;
-//     }
-//     if (name === restaurantArray[i].name) {
-//       latLngBounds[1] = latLng;
-//       console.log("Rest: " + name);
-//       break;
-//     }
-//   }
+//   // // Check to see if name is an attraction name or restaurant name
+//   // for (let i = 0; i < attractionArray.length; i++) {
+//   //   if (name === attractionArray[i].name) {
+//   //     latLngBounds[0] = latLng;
+//   //     console.log("Attr: " + name);
+//   //     break;
+//   //   }
+//   //   if (name === restaurantArray[i].name) {
+//   //     latLngBounds[1] = latLng;
+//   //     console.log("Rest: " + name);
+//   //     break;
+//   //   }
+//   // }
 
-//   let bounds = new google.maps.LatLngBounds();
-//   for (let i = 0; i < latLngBounds.length; i++) {
-//     bounds.extend(latLngBounds[i]);
-//   }
-//   map.fitBounds(bounds);
+//   // let bounds = new google.maps.LatLngBounds();
+//   // for (let i = 0; i < latLngBounds.length; i++) {
+//   //   bounds.extend(latLngBounds[i]);
+//   // }
+//   // map.fitBounds(bounds);
 // }
 
 /*
@@ -455,8 +457,8 @@ TODO:
 - Marker work
 - Attr pics?
 - Rest hours. Display as a small popup?
-- Written directions under map
 - Use the Matrix API to calc the distance between the current attr/rest and the others. Display distances 
 	beside name in others section
 - Bring search bar back. Ask user if searched item is rest/attr
+- Markers can be two attr/rest. Doesnt have to be 1 of each. Add additional markers?
 */
